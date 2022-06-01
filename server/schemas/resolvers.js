c7baf63f1fa1a34ab8ebd7408bd8711b081c6668
Jaxon Adams <jadamsresume1@gmail.com>
2022-06-01 13:44:51 -0600

    set up apollo client in front end

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -43,6 +43,17 @@ const resolvers = {
                 return updatedUser;
             }
             throw new AuthenticationError('You are not logged in');
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You are not logged in');
         }
     }
 };
